23aa1f1eb77da169edf218afe1e960ab4bd8e49e
bgrimmer <bdg79@cornell.edu>
2017-09-03 16:44:02 -0700

    Added Interactive PGSG file, which should replace everything else here. I'll come back to iterate on it later to improve its quality.

--- PGSG.jl
+++ PGSG.jl
@@ -3,7 +3,7 @@ Implementation of PGSG
 """
 module PGSG
 
-export pgsg, pf_pgsg, RPCA_PGSG, RPCA_PF_PGSG
+export pgsg, pf_pgsg, RPCA_PGSG, RPCA_PF_PGSG, RPCA_subgradient
 
 
 
@@ -18,7 +18,7 @@ function innerIterations(t, gamma, rho)
 end
 "Compute step sizes"
 function stepsize(t, j, gamma, rho)
-    return 2*gamma/((1-gamma*rho)*(j+1) + 12/(1-gamma*rho))
+    return 2*gamma/((1-gamma*rho)*(j+1) + 12/(1-gamma*rho))/17 #/2.65 *(t+1)^(-0.5)
 end
 
 
@@ -49,8 +49,8 @@ end
 function pf_pgsg(x0, grad_oracle, proj, beta, T)
     x = x0
     for t in 0:T-2
-        gamma = (t+1)^(beta)/10
-        rho = 1/(2*gamma)
+        rho = (t+1)^(beta)
+        gamma = 1/(2*rho)
         x = stochasticGradient(x, grad_oracle, proj, gamma, rho, t, innerIterations(t, gamma, rho))
     end
     return x
@@ -69,28 +69,33 @@ function stochasticGradient(y0, grad_oracle, proj, gamma, rho, t, numIterations,
     for j in 0:numIterations-2
         y = proj(y - stepsize(t,j,gamma,rho)*(grad_oracle(y) + (y-y0)/gamma))
         w = ((j+1)*w + y)/(j+2)
-        inner_obj[currentIterate] = objective(y)
+        inner_obj[currentIterate] = objective(w)
         currentIterate = currentIterate +1
     end
-    println("Inner loop finished ", norm(y0-w)/gamma)
+    println("Inner loop finished ", objective(w))
     return w, currentIterate
 end
 "WITH OBJECTIVE TRACKING - Run Parameter Free PGSG"
 function pf_pgsg(x0, grad_oracle, proj, beta, T, objective)
-    obj = zeros(T-1)
-    grad_size = zeros(T-1)
-    inner_obj = zeros(convert(Int64, (T-1)*(T-2)/2))
+    obj = zeros(convert(Int64, (T-1)*(T-2)/2))
     x = x0
     currentIterate = 1
     for t in 0:T-2
-        gamma = (t+1)^(beta)/10
-        rho = 1/(2*gamma)
-        w,currentIterate = stochasticGradient(x, grad_oracle, proj, gamma, rho, t, innerIterations(t, gamma, rho), inner_obj, objective, currentIterate)
-        grad_size[t+1] = norm(w-x)/gamma
-        x = w
-        obj[t+1] = objective(x)
+        rho = (t+1)^(beta)
+        gamma = 1/(2*rho)
+        x,currentIterate = stochasticGradient(x, grad_oracle, proj, gamma, rho, t, innerIterations(t, gamma, rho), obj, objective, currentIterate)
     end
-    return x, obj, inner_obj, grad_size
+    return x, obj
+end
+"WITH OBJECTIVE TRACKING - Run PGSG"
+function pgsg(x0, grad_oracle, proj, gamma, rho, T, objective)
+    obj = zeros(convert(Int64, (T-1)*(T-2)/2))
+    x = x0
+    currentIterate = 1
+    for t in 0:T-2
+        x,currentIterate = stochasticGradient(x, grad_oracle, proj, gamma, rho, t, innerIterations(t, gamma, rho), obj, objective, currentIterate)
+    end
+    return x, obj
 end
 
 
@@ -112,41 +117,11 @@ function oneNorm(A)
     return r
 end
 "Application to solving Robust PCA"
-function RPCA_PGSG(U,V,A, gamma, rho, T)
-    
-    #### Computes a full subgradient, fairly slow since it uses the full dataset ####
-    function deterministic_subgradient(X)
-        U,V = X[1],X[2]
-        R = U'*V
-        
-        n,m = size(U)
-        n,k = size(V)
-        ∇U = zeros(n,m)
-        ∇V = zeros(n,k)
-        
-        for i in 1:m #iterate over pixels
-            for j in 1:k #iterate over frames
-                if R[i,j] - A[i,j]>0.001
-                    for l in 1:n #iterate over components
-                        ∇U[l,i] = ∇U[l,i] + V[l,j]
-                        ∇V[l,j] = ∇V[l,j] + U[l,i]
-                    end
-                end
-                if R[i,j] - A[i,j]<-0.001
-                    for l in 1:n
-                        ∇U[l,i] = ∇U[l,i] - V[l,j]
-                        ∇V[l,j] = ∇V[l,j] - U[l,i]
-                    end
-                end
-            end
-        end
-        return [∇U, ∇V] 
-    end
+function RPCA_PGSG(U,V,A, gamma, rho, T, scale=1)
 
     #### Computes a subgradient from a random frame ####
     function stochastic_subgradient1(X)
         U,V = X[1],X[2]
-        R = U'*V
         
         n,m = size(U)
         n,k = size(V)
@@ -155,142 +130,102 @@ function RPCA_PGSG(U,V,A, gamma, rho, T)
         
         for i in 1:m #iterate over pixels
             j = rand(1:k) #random over frames
-                if R[i,j] - A[i,j]>0.001
+                if (U[:,i]'V[:,j])[1] - A[i,j]>0.001
                     for l in 1:n #iterate over components
                         ∇U[l,i] = ∇U[l,i] + V[l,j]
                         ∇V[l,j] = ∇V[l,j] + U[l,i]
                     end
                 end
-                if R[i,j] - A[i,j]<-0.001
-                    for l in 1:n #iterate over components
-                        ∇U[l,i] = ∇U[l,i] - V[l,j]
-                        ∇V[l,j] = ∇V[l,j] - U[l,i]
-                    end
-                end
-        end
-        return [∇U, ∇V] 
-    end
-
-    #### Computes a subgradient from a random frame for a random component ####    
-    function stochastic_subgradient2(X)
-        U,V = X[1],X[2]
-        R = U'*V
-        
-        n,m = size(U)
-        n,k = size(V)
-        ∇U = zeros(n,m)
-        ∇V = zeros(n,k)
-        
-        for i in 1:m #iterate over pixels
-            j = rand(1:k) #random over frames
-                if R[i,j] - A[i,j]>0.001
-                    l = rand(1:n) #random over components
-                        ∇U[l,i] = ∇U[l,i] + V[l,j]
-                        ∇V[l,j] = ∇V[l,j] + U[l,i]
-                end
-                if R[i,j] - A[i,j]<-0.001
-                    l = rand(1:n) #random over components
-                        ∇U[l,i] = ∇U[l,i] - V[l,j]
-                        ∇V[l,j] = ∇V[l,j] - U[l,i]
-                end
-        end
-        return [∇U, ∇V] 
-    end
-
-    return pf_pgsg([U,V], stochastic_subgradient2, identity, gamma, rho, T)
-end
-
-
-"Application to solving Robust PCA"
-function RPCA_PF_PGSG(U,V,A, beta, T)
-    #### Computes a full subgradient, fairly slow since it uses the full dataset ####
-    function deterministic_subgradient(X)
-        U,V = X[1],X[2]
-        R = U'*V
-        
-        n,m = size(U)
-        n,k = size(V)
-        ∇U = zeros(n,m)
-        ∇V = zeros(n,k)
-        
-        for i in 1:m #iterate over pixels
-            for j in 1:k #iterate over frames
-                if R[i,j] - A[i,j]>0.001
-                    for l in 1:n #iterate over components
-                        ∇U[l,i] = ∇U[l,i] + V[l,j]
-                        ∇V[l,j] = ∇V[l,j] + U[l,i]
-                    end
-                end
-                if R[i,j] - A[i,j]<-0.001
-                    for l in 1:n
-                        ∇U[l,i] = ∇U[l,i] - V[l,j]
-                        ∇V[l,j] = ∇V[l,j] - U[l,i]
-                    end
-                end
-            end
-        end
-        return [∇U, ∇V] 
-    end
-
-    #### Computes a subgradient from a random frame ####
-    function stochastic_subgradient1(X)
-        U,V = X[1],X[2]
-        R = U'*V
-        
-        n,m = size(U)
-        n,k = size(V)
-        ∇U = zeros(n,m)
-        ∇V = zeros(n,k)
-        
-        for i in 1:m #iterate over pixels
-            j = rand(1:k) #random over frames
-                if R[i,j] - A[i,j]>0.001
-                    for l in 1:n #iterate over components
-                        ∇U[l,i] = ∇U[l,i] + V[l,j]
-                        ∇V[l,j] = ∇V[l,j] + U[l,i]
-                    end
-                end
-                if R[i,j] - A[i,j]<-0.001
+                if (U[:,i]'V[:,j])[1] - A[i,j]<-0.001
                     for l in 1:n #iterate over components
                         ∇U[l,i] = ∇U[l,i] - V[l,j]
                         ∇V[l,j] = ∇V[l,j] - U[l,i]
                     end
                 end
         end
-        return [∇U, ∇V] 
-    end
-
-    #### Computes a subgradient from a random frame for a random component ####    
-    function stochastic_subgradient2(X)
-        U,V = X[1],X[2]
-        R = U'*V
-        
-        n,m = size(U)
-        n,k = size(V)
-        ∇U = zeros(n,m)
-        ∇V = zeros(n,k)
-        
-        for i in 1:m #iterate over pixels
-            j = rand(1:k) #random over frames
-                if R[i,j] - A[i,j]>0.001
-                    l = rand(1:n) #random over components
-                        ∇U[l,i] = ∇U[l,i] + V[l,j]
-                        ∇V[l,j] = ∇V[l,j] + U[l,i]
-                end
-                if R[i,j] - A[i,j]<-0.001
-                    l = rand(1:n) #random over components
-                        ∇U[l,i] = ∇U[l,i] - V[l,j]
-                        ∇V[l,j] = ∇V[l,j] - U[l,i]
-                end
-        end
-        return [∇U, ∇V] 
+        return [∇U, ∇V] *scale
     end
 
     function objective_RPCA(X)
         return oneNorm(X[1].'*X[2] - A)
     end
-    return pf_pgsg([U,V], stochastic_subgradient2, identity, beta, T, objective_RPCA)
+    return pgsg([U,V], stochastic_subgradient1, identity, gamma, rho, T, objective_RPCA)
 end
 
+
+"Application to solving Robust PCA"
+function RPCA_PF_PGSG(U,V,A, beta, T, scale=1)
+    #### Computes a subgradient from a random frame ####
+    function stochastic_subgradient1(X)
+        U,V = X[1],X[2]
+        
+        n,m = size(U)
+        n,k = size(V)
+        ∇U = zeros(n,m)
+        ∇V = zeros(n,k)
+        
+        for i in 1:m #iterate over pixels
+            j = rand(1:k) #random over frames
+                if (U[:,i]'V[:,j])[1] - A[i,j]>0.001
+                    for l in 1:n #iterate over components
+                        ∇U[l,i] = ∇U[l,i] + V[l,j]
+                        ∇V[l,j] = ∇V[l,j] + U[l,i]
+                    end
+                end
+                if (U[:,i]'V[:,j])[1] - A[i,j]<-0.001
+                    for l in 1:n #iterate over components
+                        ∇U[l,i] = ∇U[l,i] - V[l,j]
+                        ∇V[l,j] = ∇V[l,j] - U[l,i]
+                    end
+                end
+        end
+        return [∇U, ∇V] *scale
+    end
+
+    function objective_RPCA(X)
+        return oneNorm(X[1].'*X[2] - A)
+    end
+    return pf_pgsg([U,V], stochastic_subgradient1, identity, beta, T, objective_RPCA)
+end
+
+
+function RPCA_subgradient(U,V,A,T, scale=1)
+    function stochastic_subgradient1(X)
+        U,V = X[1],X[2]
+        R = U'*V
+        
+        n,m = size(U)
+        n,k = size(V)
+        ∇U = zeros(n,m)
+        ∇V = zeros(n,k)
+        
+        for i in 1:m #iterate over pixels
+            j = rand(1:k) #random over frames
+                if R[i,j] - A[i,j]>0.001
+                    for l in 1:n #iterate over components
+                        ∇U[l,i] = ∇U[l,i] + V[l,j]
+                        ∇V[l,j] = ∇V[l,j] + U[l,i]
+                    end
+                end
+                if R[i,j] - A[i,j]<-0.001
+                    for l in 1:n #iterate over components
+                        ∇U[l,i] = ∇U[l,i] - V[l,j]
+                        ∇V[l,j] = ∇V[l,j] - U[l,i]
+                    end
+                end
+        end
+        return [∇U, ∇V] *scale
+    end
+
+    obj = zeros(T)
+    alpha=0.01
+    X = [U,V]
+    for j in 1:T
+        X = X - (alpha/sqrt(j))*stochastic_subgradient1(X)
+        obj[j] = oneNorm(X[1].'*X[2] - A)
+        if j%50==0 println("Inner loop finished ", oneNorm(X[1].'*X[2] - A)) end
+    end
+    return X, obj
+end
 
 end #End of module PGSG
